d38d69fa4046371830b126d65412603686eb8371
T-Kobata <takuya.2441okc@gmail.com>
2022-02-05 20:19:18 +0900

    CPUの選択処理を追加

--- main.js
+++ main.js
@@ -2,10 +2,15 @@ const config = {
     "url": "https://api.recursionist.io/builder/computers?type=",  
 };
 
+// CPUの情報をAPIから取得
+const getCpuInfo = () => fetch(config.url + "cpu").then(response => response.json());
+
+// GPUの情報をAPIから取得
+const getGpuInfo = () => fetch(config.url + "gpu").then(response => response.json());
+
 // Step1 : Select Your CPU
-// ブランドとモデルを設定
-fetch(config.url + "cpu").then(response => response.json())
-.then(data => {
+// CPUのBrandを選択可能にする
+getCpuInfo().then(data => {
     // 重複を排除
     const brands = Array.from(new Set(
         data.map((obj) => { return obj.Brand; })
@@ -18,27 +23,26 @@ fetch(config.url + "cpu").then(response => response.json())
             <option value="${value}">${value}</option>
         `;
     }
+});
 
-    document.getElementById("cpu-brand").addEventListener("change", () => {
-        const brand = document.getElementById("cpu-brand").value;
-        document.getElementById("storage-model").value = brand;
-        const models = Array.from(new Set(
-            data.map((obj) => {
-                if (obj.Brand === brand)
-                return obj.Model; 
-            })
-        ));
-        document.getElementById("gpu-model").value = brand;
+// CPUのBrandを選択すると、Modelの選択を可能にする
+document.getElementById("cpu-brand").addEventListener("change", () => {
+    const brand = document.getElementById("cpu-brand").value;
+
+    getCpuInfo().then(data => {
+        const models = data.filter(obj => { return obj.Brand === brand; });
 
         const cpuModel = document.getElementById("cpu-model");
         // optionに追加
+        cpuModel.innerHTML = "";
         for (const value of models) {
             cpuModel.innerHTML += `
-                <option value="${value}">${value}</option>
+                <option value="${value.Model}">${value.Model}</option>
             `;
         }
     });
 });
+
 
 // Brandが選択されたら、そのBrandのModelを表示させる
 
