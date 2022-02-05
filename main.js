1f33d290d53e101823cbd0150a644b8955d7e2a6
T-Kobata <takuya.2441okc@gmail.com>
2022-02-05 20:25:28 +0900

    GPUの選択処理を追加

--- main.js
+++ main.js
@@ -43,33 +43,41 @@ document.getElementById("cpu-brand").addEventListener("change", () => {
     });
 });
 
-
-// Brandが選択されたら、そのBrandのModelを表示させる
-
-const getCpuBrand = (data) => {
-    const div = document.createElement("div");
-    for (const key in data) {
-        div.innerHTML += `
-            <option value="${data[key].Brand}">${data[key].Brand}</option>
-        `;
-    }
-    return div;
-}
-
-const putCpuBrand = (cpuBrand, data) => {
+// Step2 : Select Your GPU
+// GPUのBrandを選択可能にする
+getGpuInfo().then(data => {
+    // 重複を排除
     const brands = Array.from(new Set(
-        data.map((obj) => { return obj.Brand })
+        data.map((obj) => { return obj.Brand; })
     ));
+    document.getElementById("storage-model").value = data.length;
 
+    const gpuBrand = document.getElementById("gpu-brand");
+    // optionに追加
     for (const value of brands) {
-        cpuBrand.innerHTML += `
+        gpuBrand.innerHTML += `
             <option value="${value}">${value}</option>
         `;
     }
-}
+});
 
+// GPUのBrandを選択すると、Modelの選択を可能にする
+document.getElementById("gpu-brand").addEventListener("change", () => {
+    const brand = document.getElementById("gpu-brand").value;
 
-// Step2 : Select Your GPU
+    getGpuInfo().then(data => {
+        const models = data.filter(obj => { return obj.Brand === brand; });
+
+        const gpuModel = document.getElementById("gpu-model");
+        // optionに追加
+        gpuModel.innerHTML = "";
+        for (const value of models) {
+            gpuModel.innerHTML += `
+                <option value="${value.Model}">${value.Model}</option>
+            `;
+        }
+    });
+});
 
 // Step3 : Select Your Memory Card
 document.querySelectorAll(".add-btn")[0].addEventListener("click", () => {
